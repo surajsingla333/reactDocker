3c624c1c889d3ee233f0e941dda80fee1721cb6e
Suraj Singla <surajsingla333@gmail.com>
2020-06-02 00:48:29 +0530

    from azure

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          YOO from the feature Branch!!!
+          Hello from Azure Service!!!
         </p>
         <a
           className="App-link"
