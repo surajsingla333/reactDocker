ab6b0b97555c77e489d70c253f14c89cab08dece
Suraj Singla <surajsingla333@gmail.com>
2020-06-02 13:30:31 +0530

    CI/CD testing

--- src/App.js
+++ src/App.js
@@ -7,8 +7,7 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Hello from Azure Service!!!
+          Hello from Azure Service - CI/CD testing!!!
         </p>
         <a
           className="App-link"
