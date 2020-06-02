04b13982364aeee3883e2623f862e6a36ef78433
Suraj Singla <surajsingla333@gmail.com>
2020-06-02 13:39:03 +0530

    CI/CD working

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>  Hello from Azure Service - CI/CD testing!!! </p>
+        <p>  Hello from Azure Service - CI/CD testing2!!! </p>
         <a
           className="App-link"
           href="https://reactjs.org"
